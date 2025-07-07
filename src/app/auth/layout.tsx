176cd4e012b8117970f0e59e3dd65d33b35641a9
dang khoa <canonemire19@gmail.com>
2025-07-07 13:10:41 +0700

    Update layout.tsx

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -18,7 +18,7 @@ export default function AuthLayout({
           <div className="flex justify-center">
             <Image
               src="/farmer.svg"
-              alt="Firefighter icon"
+              alt="Farmer icon"
               width={192}
               height={192}
               className="w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48" // Removed mb-4 as space-y on parent will handle it
